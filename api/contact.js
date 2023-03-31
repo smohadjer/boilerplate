6774efeee7a78ac2ea202e2971939688911749b1
Saeid Mohadjer <saeid@fastmail.fm>
2023-03-31 10:13:25 +0200

    adds api folder

---
+++ api/contact.js
@@ -0,0 +1,16 @@
+export default function handler(req, res) {
+  /*
+  if (req.body) {
+    return res.send(`Hello ${req.body.username}!`);
+  } else {
+    return res.send(`Hello ${req.query.username}!`);
+  }
+  */
+  console.log('form handler invoked');
+
+  if (req.body) {
+    res.setHeader('Content-Type', 'text/plain')
+    res.write('you posted:\n')
+    res.end(JSON.stringify(req.body, null, 2))
+  }
+}
